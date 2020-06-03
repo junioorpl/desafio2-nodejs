e7405d9d95ae1d9f5565153d8ac970afc54cc52b
Eurípedes Júnior <junior.jpl@outlook.com>
2020-06-03 20:29:51 +0000

    Second commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO{
+  title: string;
+  value: number;
+  type: 'income'|'outcome';
+}
+
+interface Receipt{
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,42 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): Receipt {
+    const balance = this.getBalance();
+    const transactions = this.transactions;
+
+    return {transactions, balance};
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+
+    if(this.transactions.length<=0)
+        return {income:0, outcome:0, total:0};
+
+    if(this.transactions.filter(t => t.type === "income").length>0)
+    income = this.transactions.filter(t => t.type === "income").map(t => t.value).reduce((a,b)=>a+b);
+
+    if(this.transactions.filter(t => t.type === "outcome").length>0)
+    outcome = this.transactions.filter(t => t.type === "outcome").map(t => t.value).reduce((a,b)=>a+b);
+
+    const balance = {
+        income,
+        outcome,
+        total: income-outcome,
+    }
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: TransactionDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO{
+  title: string;
+  value: number;
+  type: 'income'|'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,15 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(data: TransactionDTO): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if(data.type==='outcome' && data.value>balance.total)
+      throw Error('Insufficient balance');
+
+    const transaction = this.transactionsRepository.create(data);
+
+    return transaction;
   }
 }
 
